6e77a03c42aa25ff81a40e51f02d6be75573d246
Alice Goulley <goulleyalice@gmail.com>
2020-12-05 22:58:12 +0100

    Exercice du pendu

--- Tonnom.py
+++ Tonnom.py
@@ -1,12 +1,10 @@
 print('Quel est ton nom?')
 monNom=input()
-y=str(len(monNom))
 print('Nice to meet you, '+ monNom)
-print('La longueur de ton nom est de ' + y + ' caracteres') 
+print('La longueur de ton nom est de ' + str(len(monNom)) + ' caracteres') 
 print('Quel est ton age?')
 monAge=input()
-x=int(monAge)
-if x<35:
+if int(monAge)<35:
     print('Tu es jeune!')
 else:
     print('Ok boomer')
